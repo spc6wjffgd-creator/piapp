0135a5447e0e102ae4f6609f68d6f130b6aa3221
joypapadata <taiji1996@naver.com>
2026-05-09 21:20:33 +0900

    Add files via upload

---
+++ music-library.js
@@ -0,0 +1,20 @@
+window.MUSIC_LIBRARY = [
+  {
+    title: "Warm Intro",
+    artist: "Studio Demo",
+    category: "Chill",
+    src: "./music/warm-intro.mp3",
+  },
+  {
+    title: "Night Drive",
+    artist: "Studio Demo",
+    category: "Lo-fi",
+    src: "./music/night-drive.mp3",
+  },
+  {
+    title: "Morning Groove",
+    artist: "Studio Demo",
+    category: "Pop",
+    src: "./music/morning-groove.mp3",
+  },
+];
